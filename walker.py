91bfead08e9919fbc6d63c6b1bce5913b94293d6
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2019-04-12 14:41:50 -0600

    Adding walker example

---
+++ walker.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import gym
+import time
+
+env = gym.make('BipedalWalker-v2')
+
+for i_episode in range(100):
+    observation = env.reset()
+    for t in range(10000):        
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("{} timesteps taken for the episode".format(t+1))
+            break
+    #time.sleep(0.1)
+    
+env.close()
